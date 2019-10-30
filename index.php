0111f67771f7622a53e0db032e155dd7cceec3b6
Sydorov Leonid <info@space-ship.xyz>
2019-10-30 18:37:04 +0200

    Init front-end files

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<title>Simpe SEO analyzer</title>
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="/css/styles.css" />
+</head>
+
+<body>
+	
+	<script type="text/javascript" src="/js/scripts.js"></script>
+</body>
+</html>
